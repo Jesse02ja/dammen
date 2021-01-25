7b9ab33a1ad06c5164ac1fded4f2ca1211019d10
JerryvS <jeroen.d.vanstenis@gmail.com>
2021-01-25 18:37:08 +0100

    changing white to left

--- dammen/bord.py
+++ dammen/bord.py
@@ -5,7 +5,7 @@ class Bord:
     def __init__(self):
         self.bord = []
         self.gekozen_stuk = None
-        self.zwart_left = self.zwart_left = 20
+        self.zwart_left = self.wit_left = 20
         self.zwart_dam = self.wit_dam = 0
     
     def vierkant(self,scherm):
